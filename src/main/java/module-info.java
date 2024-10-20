607c52f0988d9617e803039d483e025138d79ba2
Dang Dinh Khang <lhang18022005@gmai.com>
2024-10-20 07:56:47 +0700

    set JDBC

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module uet.librarymanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
+    requires org.slf4j;
 
     opens uet.librarymanagementsystem.controllers to javafx.fxml;
     exports uet.librarymanagementsystem;
