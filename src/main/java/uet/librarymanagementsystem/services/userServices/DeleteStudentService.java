cd2a750e52b1340cca451fec9febd03e27208b79
Dang Dinh Khang <lhang18022005@gmai.com>
2024-11-08 23:40:25 +0700

    fix loi java

--- src/main/java/uet/librarymanagementsystem/services/userServices/DeleteStudentService.java
+++ src/main/java/uet/librarymanagementsystem/services/userServices/DeleteStudentService.java
@@ -1,14 +1,12 @@
-//package uet.librarymanagementsystem.services.userServices;
-//
-//import uet.librarymanagementsystem.DatabaseOperation.UserDO;
-//
-//public class DeleteStudentService {
-//    public static boolean deleteStudentByID(String ID) {
-//        if (UserDO.isUserExists(ID)) {
-//            System.out.println("Sinh viên với ID " + ID + " đã tồn tại.");
-//            return false;
-//        }
-//
-//
-//    }
-//}
+package uet.librarymanagementsystem.services.userServices;
+import uet.librarymanagementsystem.DatabaseOperation.UserDO;
+
+public class DeleteStudentService {
+    public static boolean deleteStudentByID(String ID) {
+        if (UserDO.isUserExists(ID)) {
+            System.out.println("Sinh viên với ID " + ID + " đã tồn tại.");
+            return false;
+        }
+        return false;
+    }
+}
