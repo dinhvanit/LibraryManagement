bec7efc492afc9d4a17473c6e1befb0878a787c5
Duong Thai Tran <duongthaitran68@gmail.com>
2024-11-17 07:59:24 +0700

    add method api:

--- src/main/java/uet/librarymanagementsystem/services/documentServices/LibraryManagementSystem.java
+++ src/main/java/uet/librarymanagementsystem/services/documentServices/LibraryManagementSystem.java
@@ -1,35 +1,19 @@
 package uet.librarymanagementsystem.services.documentServices;
 
-import org.json.JSONObject;
-
 public class LibraryManagementSystem {
-
-    /**
-     * Phương thức để hiển thị thông tin sách dựa trên ISBN.
-     * @param isbn Mã ISBN của sách.
-     */
-    public void displayBookInfoByISBN(String isbn) {
-        JSONObject bookInfo = BookLookupService.fetchBookInfoByISBN(isbn);
-
-        if (bookInfo != null) {
-
-            String title = bookInfo.optString("title", "N/A");
-            String authors = bookInfo.optJSONArray("authors") != null ? bookInfo.getJSONArray("authors").join(", ") : "N/A";
-            String publisher = bookInfo.optString("publisher", "N/A");
-            String description = bookInfo.optString("description", "N/A");
-
-
-            System.out.println("Title: " + title);
-            System.out.println("Authors: " + authors);
-            System.out.println("Publisher: " + publisher);
-            System.out.println("Description: " + description);
-        } else {
-            System.out.println("No information found for ISBN: " + isbn);
-        }
-    }
-
     public static void main(String[] args) {
-        LibraryManagementSystem lms = new LibraryManagementSystem();
-        lms.displayBookInfoByISBN("1551640260");
+        BookLookupService bookLookupService = new BookLookupService("1551640260");
+        if (bookLookupService.checkBookInfoByISBN()) {
+            System.out.println(bookLookupService.getIsbn());
+            System.out.println(bookLookupService.getTitleBook());
+            System.out.println(bookLookupService.getAllAuthors());
+            System.out.println(bookLookupService.getTheFirstAuthor());
+            System.out.println(bookLookupService.getAllCategories());
+            System.out.println(bookLookupService.getTheFirstCategory());
+            System.out.println(bookLookupService.getDescription());
+            System.out.println(bookLookupService.getLanguage());
+            System.out.println(bookLookupService.getPreviewLink());
+            System.out.println(bookLookupService.getThumbnailUrl());
+        }
     }
 }
